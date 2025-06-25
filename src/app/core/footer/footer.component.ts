edc38118e840e41f0013c7e96f66f68c4a642105
jalilSMR <jalil.mendoza.r@gmail.com>
2025-06-25 14:13:45 -0600

    footer agregado

---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  footerLinks$: Observable<{ text: string; url: string }[]> = of([
+    { text: 'Términos y condiciones', url: '#' },
+    { text: 'Política de privacidad', url: '#' }
+  ]);
+}
